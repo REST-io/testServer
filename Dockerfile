5a96adef67f482b073a2418df46d88555f17d873
flydog98 <guinnessoverflow@gmail.com>
2023-11-04 23:21:26 +0900

    feat: 빌드용 파일

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM guinnessoverflow/rest.io-os:1.2
+
+ENV PATH="/root/.nvm/versions/node/v21.1.0/bin:${PATH}"
---
+++ build.sh
@@ -0,0 +1,6 @@
+# 이미지 빌드
+docker build -t guinnessoverflow/rest.io-os:1.2 .
+
+# 이미지 push
+docker push guinnessoverflow/rest.io-os:1.2
+
