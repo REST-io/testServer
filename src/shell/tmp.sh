bb5067efd17d6cac436376da48e2a766e37762c6
flydog98 <guinnessoverflow@gmail.com>
2023-11-04 23:21:05 +0900

    feat: tmp.sh org1.yaml 절대 경로로 업데이트

--- src/shell/tmp.sh
+++ src/shell/tmp.sh
@@ -1,5 +1,5 @@
 # 실행하고자 하는 명령 예시
-kubectl hlf chaincode invoke --config=org1.yaml \
+kubectl hlf chaincode invoke --config=/root/org1.yaml \
     --user=admin --peer=org1-peer0.default \
     --chaincode=asset --channel=demo \
     --fcn=$1 -a '[]'
