6a06b1d49f6333248f0c9bf73fd2ab87277a376f
flydog98 <guinnessoverflow@gmail.com>
2023-11-15 12:46:00 +0900

    feat: body에 피어 노드 지정 가능
    body 예시
    
    {
        codeName: CreateCar,
        argument: [
            CAR123,
            Rey,
            iOS,
            Red,
            MoonJS
        ],
        node: org1-peer1
    }

--- src/app/app.service.js
+++ src/app/app.service.js
@@ -4,7 +4,7 @@ import { getAbsolutePath } from "../util/path.js";
 export const executeDeploymentScripts = async (req, res) => {
   try {
     const path = getAbsolutePath("../shell/tmp.sh");
-    const { codeName, argument } = req.body;
+    const { codeName, argument, node } = req.body;
 
     const argumentLine = argument
       .reduce((acc, curr) => {
@@ -12,7 +12,7 @@ export const executeDeploymentScripts = async (req, res) => {
       }, "")
       .trim();
 
-    const command = `bash "${path}" ${codeName} "${argumentLine}"`;
+    const command = `bash "${path}" ${codeName} "${argumentLine}" ${node}`;
     await executeCommand(command); // bash 스크립트를 직접 실행합니다.
 
     res.status(201).json({ message: "배포 스크립트 실행 완료" });
--- src/shell/tmp.sh
+++ src/shell/tmp.sh
@@ -2,6 +2,6 @@
 
 # 실행하고자 하는 명령 예시
 kubectl hlf chaincode invoke --config=/home/ubuntu/org1.yaml \
-    --user=admin --peer=org1-peer0.default \
+    --user=admin --peer=$3.default \
     --chaincode=asset --channel=demo \
     --fcn=$1 $2
